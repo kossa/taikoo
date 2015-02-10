18aa8f53eb22eaeaf5e5ac4f4198f2c84dd771eb
kossa <hadjikouceyla@gmail.com>
2015-02-10 18:00:48 +0100

    Header/Navigation - Navicon

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 (function($){
 
+    // Click on collpase menu in mobile
+    $(".navbar-toggle").click(
+        function(){
+            $(this).toggleClass('open');
+        }
+    );
+
+    // Show/Hide the form for mobile
     $("#show-form").click(
         function(){
             
