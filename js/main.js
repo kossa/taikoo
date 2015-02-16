0ea7958e455f45f8a40931c39fc1b2fa7bdac7a5
kossa <hadjikouceyla@gmail.com>
2015-02-16 14:35:24 +0100

    Fix all issues - before changing slider

--- js/main.js
+++ js/main.js
@@ -1,5 +1,69 @@
 (function($){
 
+    // Slide paused
+    $('#carousel-kossa').carousel({interval: false});
+
+    // Swipe event
+    $('#carousel-kossa').swipe( {
+        //Generic swipe handler for all directions
+        swipe:function(event, direction, distance, duration, fingerCount, fingerData) {
+            _dir = (direction == 'left') ? 'next' : 'prev' ;
+          $(this).carousel( _dir );  
+        },
+        //Default is 75px, set to 0 for demo so any distance triggers swipe
+         threshold:0
+      });
+
+
+
+    // Click on play button
+    $(".wrapper a.play").click(
+        function(){
+
+            var
+                _this    = $(this),
+                _wrapper = _this.parent(),
+                iframe   = _wrapper.next('iframe')[0],
+                player   = $f(iframe),
+                status   = $('.status');
+
+            // Hide the Wrapper
+            _wrapper.fadeOut("slow", function(){
+                _wrapper.remove();
+            })
+
+            // When the player is ready, add listeners for pause, finish, and playProgress
+            player.addEvent('ready', function() {
+                status.text('ready');
+                
+                player.addEvent('pause', onPause);
+                player.addEvent('finish', onFinish);
+                player.addEvent('playProgress', onPlayProgress);
+            });
+
+            player.api('play');
+
+            $('#carousel-kossa').on('slide.bs.carousel', function(){
+                player.api('pause');
+            })
+
+
+            function onPause(id) {
+                status.text('paused');
+            }
+
+            function onFinish(id) {
+                status.text('finished');
+            }
+
+            function onPlayProgress(data, id) {
+                status.text(data.seconds + 's played');
+            }
+
+
+        }
+    );
+
     // Click on collpase menu in mobile
     $(".navbar-toggle").click(
         function(){
@@ -20,5 +84,11 @@
             return false;
         }
     );
+
+/*
+|------------------------------------------------------------------------------------
+| Fonctions
+|------------------------------------------------------------------------------------
+*/
 
 })(jQuery);
