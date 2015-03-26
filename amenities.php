43f3173d7f098f35aaa28cfd3a3cf5ce802151a8
kossa <hadjikouceyla@gmail.com>
2015-03-26 08:06:42 +0100

    Amenties - Put the new text for all version

--- amenities.php
+++ amenities.php
@@ -15,7 +15,6 @@
             <div class="wrapper">
               <h4>Backed by Swire Properties’ established tradition of impeccable service, the 28-storey Taikoo Place Apartments presents 111 exclusive homes optimised for comfort, space and efficiency. </h4>
               <p>The building’s podium façade — a striking juxtaposition of glass, stone, and aluminium provides visibility for the restaurant and privacy for the residential lobby simultaneously. The residential lobby creates a monolithic, quiet, and elegant space contrasting the active street, using hand carved graphite stone for the walls, high quality European wood flooring, and a magnificent chandelier that is soft and welcoming. The funnel shape of the lobby creates a natural movement towards the elevators whilst transforming the speed of the street to a slow passage. The podium is truly brought to life by the clubhouse balcony, defined by the sculptural stone benches and private outdoor dining.</p>
-              
               <p>From the sophisticated residences, to the finest amenities, to the efficient service, Taikoo Place Apartments brings contemporary comfort at its best to Taikoo Place within a single, unforgettable location.</p>
             </div>
           </div>
@@ -31,10 +30,10 @@
                 
           <div class="second">
             <div class="wrapper">
-              <h3>OUR revitalizing CLUBHOUSE</h3>
+              <h3>OUR REVITALIZING CLUBHOUSE</h3>
               <h4>Exclusively occupying the podium's top floor, the clubhouse at Taikoo Place Apartments is a truly unique haven for healthy living.</h4>
               <p>The central lounge emulates the relaxed ambience of a stylish living room, offering a refreshment bar and a reading lounge that also serves as a business centre. The clubhouse also comprises two opulent spas, a modern gym, two outdoor terraces, and laundry facilities.</p>
-              <p><small>Artist impression of reading lounge in clubhouse</small></p>
+              <p>Artist impression of reading lounge in clubhouse</p>
             </div>
           </div>
           <div class="first visible-xs">
@@ -47,7 +46,7 @@
             <div class="wrapper">
               <h3>THE SPA</h3>
               <p>Unwind from the stimulating atmosphere of Taikoo Place within our restorative spas. Our intimate spa invites singles and couples to rejuvenate in private, or share our larger spa with friends, which is furnished with heated chaises and a tranquil lounge. The spas' superb interiors also incorporate deluxe rain showers, Jacuzzi and steam rooms.</p>
-              <p><small>Artist impression of spa in clubhouse</small></p>
+              <p>Artist impression of spa in clubhouse</p>
             </div>
           </div>
           <div class="second">
